68e7760dbaa13792ea9513fc95c2513803014813
ruwadih Saud <145326394+ruwadihSaud@users.noreply.github.com>
2023-12-09 05:07:09 +0300

    UI.py

--- UI.py
+++ UI.py
@@ -160,7 +160,7 @@ with col4:
       else:
           prediction = "Lift"
       st.text("The chance is : {} ".format(prediction))
-      st.text("The probability is : %{:.1f} ".format(probability[0][1]*100,1))
+      st.text("The probability is : %{:.1f} ".format(prob))
 
 #st.text(df.head())
 st.sidebar.header("Created by:\nRuwaidiah   \nRawan   \nSara   \nShuruq   \nLujain   \nAhmed   \nMariam   \nFatima   \nRenad")
